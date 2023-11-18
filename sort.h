e95de6a09b918162a804f5146f6305c0f97a5132
deantosh <deantoshdaiddoh@gmail.com>
2023-11-18 05:23:50 -0800

    selection sort prototype added

--- sort.h
+++ sort.h
@@ -26,5 +26,8 @@ void print_list(const listint_t *list);
 
 /*Function prototypes*/
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t *a, listint_t *b);
+void selection_sort(int *array, size_t size);
 
 #endif /*SORT_H*/
